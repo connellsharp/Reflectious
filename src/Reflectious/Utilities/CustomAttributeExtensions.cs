a9230acb518652e52dfe206517707e6b47755617
Connell <connellsharp@gmail.com>
2018-08-19 17:26:47 +0100

    Added CustomAttributeExtensions

---
+++ src/Reflectious/Utilities/CustomAttributeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+using JetBrains.Annotations;
+
+namespace Reflectious
+{
+    public static class CustomAttributeExensions
+    {
+        /// <summary>
+        /// Checks whether a custom attribute of a specified type is applied to a specified member.
+        /// </summary>
+        [PublicAPI]
+        public static bool HasCustomAttribute<T>(this MemberInfo memberInfo)
+            where T : Attribute
+        {
+            return memberInfo.GetCustomAttribute<T>() != null;
+        }
+    }
+}
