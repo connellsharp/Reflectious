e943bb5af66558f33e7345bbe79d33f9d4776e7b
Connell <connellsharp@gmail.com>
2018-10-23 19:35:49 +0100

    Added a couple of extra extensions

--- src/Reflectious/Utilities/CustomAttributeExtensions.cs
+++ src/Reflectious/Utilities/CustomAttributeExtensions.cs
@@ -1,19 +1,29 @@
 ﻿using System;
+using System.Linq;
 using System.Reflection;
 using JetBrains.Annotations;
 
 namespace Reflectious
 {
-    public static class CustomAttributeExensions
+    public static class CustomAttributeExtensions
     {
         /// <summary>
         /// Checks whether a custom attribute of a specified type is applied to a specified member.
         /// </summary>
         [PublicAPI]
-        public static bool HasCustomAttribute<T>(this MemberInfo memberInfo)
-            where T : Attribute
+        public static bool HasCustomAttribute<TAttribute>(this MemberInfo method)
+            where TAttribute : Attribute
         {
-            return memberInfo.GetCustomAttribute<T>() != null;
+            return method.GetCustomAttributes<TAttribute>().Any();
+        }
+        
+        /// <summary>
+        /// Checks whether a custom attribute of a specified type is applied to a specified member.
+        /// </summary>
+        [PublicAPI]
+        public static bool HasCustomAttribute(this MemberInfo method, Type attributeType)
+        {
+            return method.GetCustomAttributes(attributeType).Any();
         }
     }
 }
---
+++ src/Reflectious/Utilities/MethodExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+
+namespace Reflectious
+{
+    public static class MethodExtensions
+    {   
+        public static Type GetGenericArgument(this MethodBase method, int index = 0)
+        {
+            return method.GetGenericArguments()[index];
+        }
+    }
+}
